16c7588a9daff99199f7b62eee4fa4a1f82f05e3
Kris Marshall <rflctnofu@hotmail.com>
2022-08-27 11:17:32 -0400

    I think I got removeBook in correctly...cant check yet until I get the login fixed,

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -14,7 +14,7 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
-  const [saveBookMute, { error }] = useMutation(SAVE_BOOK);
+  const [saveBookMut] = useMutation(SAVE_BOOK);
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
@@ -66,7 +66,7 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBookMute(bookToSave, token);
+      const response = await saveBookMut({ variables: { saveNewBook: { ...bookToSave } } });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
